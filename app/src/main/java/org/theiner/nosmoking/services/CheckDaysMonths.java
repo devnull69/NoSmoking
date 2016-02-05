b132cd8b88fa7dbc65bd65d7da62b9eb26c5f679
Thomas Theiner <thomas.theiner@gmx.de>
2016-02-05 10:14:27 +0100

    Added check for system disabled notifications

--- app/src/main/java/org/theiner/nosmoking/services/CheckDaysMonths.java
+++ app/src/main/java/org/theiner/nosmoking/services/CheckDaysMonths.java
@@ -1,5 +1,6 @@
 package org.theiner.nosmoking.services;
 
+import android.app.AppOpsManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -7,6 +8,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -16,6 +18,9 @@ import org.theiner.nosmoking.util.AlarmHelper;
 import org.theiner.nosmoking.util.DateHelper;
 import org.theiner.nosmoking.util.Tempus;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -26,6 +31,9 @@ import java.util.Calendar;
 public class CheckDaysMonths extends Service {
 
     private static int ALARM_ID = 131313;
+
+    private final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+    private final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
 
     @Override
     public void onCreate() {
@@ -78,23 +86,25 @@ public class CheckDaysMonths extends Service {
 
     public void sendNotification(String notifyText) {
 
-        Intent mainIntent = new Intent(this, OverviewActivity.class);
-        @SuppressWarnings("deprecation")
-        Notification noti = new Notification.Builder(this)
-                .setAutoCancel(true)
-                .setContentIntent(PendingIntent.getActivity(this, 131314, mainIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT))
-                .setContentTitle("Gratuliere!")
-                .setContentText("Du bist seit " + notifyText + " rauchfrei!")
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setSmallIcon(R.mipmap.icon_white)
-                .setTicker("Du bist seit " + notifyText + " rauchfrei!")
-                .setWhen(System.currentTimeMillis())
-                .getNotification();
+        if(isNotificationEnabled(this)) {
+            Intent mainIntent = new Intent(this, OverviewActivity.class);
+            @SuppressWarnings("deprecation")
+            Notification noti = new Notification.Builder(this)
+                    .setAutoCancel(true)
+                    .setContentIntent(PendingIntent.getActivity(this, 131314, mainIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT))
+                    .setContentTitle("Gratuliere!")
+                    .setContentText("Du bist seit " + notifyText + " rauchfrei!")
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setSmallIcon(R.mipmap.icon_white)
+                    .setTicker("Du bist seit " + notifyText + " rauchfrei!")
+                    .setWhen(System.currentTimeMillis())
+                    .getNotification();
 
-        NotificationManager notificationManager
-                = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(131315, noti);
+            NotificationManager notificationManager
+                    = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(131315, noti);
+        }
 
     }
 
@@ -102,5 +112,42 @@ public class CheckDaysMonths extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private boolean isNotificationEnabled(Context context) {
+
+        AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+
+        ApplicationInfo appInfo = context.getApplicationInfo();
+
+        String pkg = context.getApplicationContext().getPackageName();
+
+        int uid = appInfo.uid;
+
+        Class appOpsClass = null; /* Context.APP_OPS_MANAGER */
+
+        try {
+
+            appOpsClass = Class.forName(AppOpsManager.class.getName());
+
+            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
+
+            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+            int value = (int)opPostNotificationValue.get(Integer.class);
+
+            return ((int)checkOpNoThrowMethod.invoke(mAppOps,value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
