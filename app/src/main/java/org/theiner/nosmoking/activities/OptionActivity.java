62f319ae9907872c7cb07f87ac9c226ae431617f
Thomas Theiner <thomas.theiner@gmx.de>
2016-06-01 16:56:57 +0200

    Datepicker added for options

--- app/src/main/java/org/theiner/nosmoking/activities/OptionActivity.java
+++ app/src/main/java/org/theiner/nosmoking/activities/OptionActivity.java
@@ -1,21 +1,34 @@
 package org.theiner.nosmoking.activities;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.theiner.nosmoking.R;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class OptionActivity extends Activity {
+
+    private final int DATEPICKER_ID = 4711;
+
+    private int year;
+    private int month;
+    private int day;
+
+    private TextView editStartDatum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,7 @@ public class OptionActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
 
-        EditText editStartDatum = (EditText) findViewById(R.id.editStartDatum);
+        editStartDatum = (TextView) findViewById(R.id.editStartDatum);
         EditText editAnzahlZiggis = (EditText) findViewById(R.id.editAnzahlZiggis);
         EditText editAnzahlInPackung = (EditText) findViewById(R.id.editAnzahlInPackung);
         EditText editKostenProPackung = (EditText) findViewById(R.id.editKostenProPackung);
@@ -41,6 +54,15 @@ public class OptionActivity extends Activity {
         int anzahlInPackung = settings.getInt("anzahlInPackung", 22);
         float kostenProPackung = settings.getFloat("kostenProPackung", 6.0f);
 
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        if(!"".equals(strStartDatum)) {
+            day = Integer.parseInt(strStartDatum.substring(0,2));
+            month = Integer.parseInt(strStartDatum.substring(3,5)) - 1;
+            year = Integer.parseInt(strStartDatum.substring(6,10));
+        }
 
         editStartDatum.setText(strStartDatum);
         editAnzahlZiggis.setText(String.valueOf(anzahlZiggis));
@@ -50,7 +72,6 @@ public class OptionActivity extends Activity {
 
     public void btnOk_Click(View view) {
         // Prüfen, ob gültiges Datum gewählt wurde
-        EditText editStartDatum = (EditText) findViewById(R.id.editStartDatum);
         EditText editAnzahlZiggis = (EditText) findViewById(R.id.editAnzahlZiggis);
         EditText editAnzahlInPackung = (EditText) findViewById(R.id.editAnzahlInPackung);
         EditText editKostenProPackung = (EditText) findViewById(R.id.editKostenProPackung);
@@ -103,4 +124,29 @@ public class OptionActivity extends Activity {
     public void btnCancel_Click(View view) {
         this.finish();
     }
+
+    public void setDate(View view) {
+        showDialog(DATEPICKER_ID);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DATEPICKER_ID) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        editStartDatum.setText(new StringBuilder().append(day<10?"0"+day:day).append(".")
+                .append(month<10?"0"+month:month).append(".").append(year));
+    }
+
 }
